0c03b00ab6a5fb755e78a1f87c81a0002732d343
Jason L Perry <jason@suncoast.io>
2019-01-15 21:36:45 -0500

    Update
    A lazy commit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ require "pagy/extras/bulma"
 
 module ApplicationHelper
   include Pagy::Frontend
+  USER_AGENT_PARSER = UserAgentParser::Parser.new
+
+  def ua(ua_string)
+    USER_AGENT_PARSER.parse(ua_string)
+  end
 end
